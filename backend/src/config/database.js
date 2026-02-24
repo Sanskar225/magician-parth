f35d2ea3863b0fa05a57b3de97bf62119c93cc16
sanskar225 <sanskarsinha225@gmail.com>
2026-02-25 01:07:08 +0530

    all database testing complete all working

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require('sequelize');
 const logger = require('../utils/logger');
-
+// console.log("ENV CHECK →", {
+//   DB_NAME: process.env.DB_NAME,
+//   DB_USER: process.env.DB_USER,
+//   DB_PASSWORD: process.env.DB_PASSWORD,
+//   DB_HOST: process.env.DB_HOST
+// });
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
